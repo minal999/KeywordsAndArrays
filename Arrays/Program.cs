61cbe8904f7ca2eaad4d8834755ee2c5bf75a0ab
minal999 <minughadge555@gmail.com>
2022-09-19 17:38:45 +0530

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] names = new string[10];
+            Console.WriteLine("Enter 10 elements");
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+            Console.WriteLine("----------------------");
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"Names {i} = {names[i]}");
+            }
+
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        //static void Swap(ref int a, ref int b)
+        //{
+        //    int temp;
+        //    temp = a;
+        //    a = b;
+        //    b = temp;
+        //}
+
+        //static void Calculation(int a, int b, out int sum, out int sub, out int multiply)
+        //{
+        //    sum = a + b;
+        //    sub = a - b;
+        //    multiply = a * b;
+        //}
+
+        static void AcceptNames(params string[] names)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine("----------------------------");
+        }
+
+
+        static void Main(string[] args)
+        {   //Ref Keyword
+
+            //int a = 10, b = 20;
+            //Swap(ref a, ref b); // call by ref / address
+            //Console.WriteLine($"a = {a}  b= {b}");
+
+
+            //out keyword
+
+            //int a = 10, b = 20, sum, multiply, sub;
+            //Calculation(a, b, out sum, out sub, out multiply);
+            //Console.WriteLine($" sum ={sum} \n sub={sub} \n multiplication {multiply}");
+
+
+
+            //params keyword
+
+            AcceptNames("Rahul", "Kishor", "Tushar");
+            AcceptNames("Rahul", "Kishor");
+            AcceptNames("Rahul", "Kishor", "Tushar", "Raj");
+
+        }
+    }
+}
